0924625f80c4ba87b48a1ddb69bcc9d40592430c
Joel Martinez <jmarti221@gmail.com>
2021-12-11 15:19:54 -0700

    Add https security to app

---
+++ src/main/java/family/themartinez/mealplanner/main/security/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package family.themartinez.mealplanner.main.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.headers().httpStrictTransportSecurity().maxAgeInSeconds(31536000).includeSubDomains(true);
+  }
+}
