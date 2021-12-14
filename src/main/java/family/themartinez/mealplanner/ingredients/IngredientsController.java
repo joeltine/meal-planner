29a3db903c8221adcf105d347a2e0b4ab3ce871b
Joel Martinez <jmarti221@gmail.com>
2021-12-13 20:58:36 -0700

    Cleanup old autocomplete css and start building autocomplete sql queries

--- src/main/java/family/themartinez/mealplanner/ingredients/IngredientsController.java
+++ src/main/java/family/themartinez/mealplanner/ingredients/IngredientsController.java
@@ -1,17 +1,29 @@
 package family.themartinez.mealplanner.ingredients;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IngredientsController {
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public IngredientsController(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
   @GetMapping("/ingredients")
   public List<Map<String, Object>> getIngredients(@RequestParam(name = "q") String query) {
-    return ImmutableList.of(ImmutableMap.of("value", 1, "text", "bar"));
+    String sql =
+        String.format("SELECT id, name FROM ingredients WHERE name LIKE '%%%s%%' LIMIT 30", query);
+    List<Map<String, Object>> results = this.jdbcTemplate.queryForList(sql);
+    ImmutableList.Builder newResults = ImmutableList.builder();
+
+    return newResults.build();
   }
 }
