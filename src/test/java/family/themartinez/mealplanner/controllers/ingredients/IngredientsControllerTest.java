7e3e60e9edc37aae8f100c71a438b18837c27d33
Joel Martinez <jmarti221@gmail.com>
2021-12-15 08:35:49 -0700

    Checkpoint in setting up IngredientsControllerTest to use SpringBootTest and inmem db

--- src/test/java/family/themartinez/mealplanner/controllers/ingredients/IngredientsControllerTest.java
+++ src/test/java/family/themartinez/mealplanner/controllers/ingredients/IngredientsControllerTest.java
@@ -6,16 +6,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(IngredientsController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @Sql({"/test-schema.sql", "/test-data.sql"})
 @ActiveProfiles("test")
-@AutoConfigureJdbc
 public class IngredientsControllerTest {
   @Autowired private MockMvc mockMvc;
 
