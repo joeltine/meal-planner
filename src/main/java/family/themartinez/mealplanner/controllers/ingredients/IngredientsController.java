062fe86c08f0ce266b5f3f8c4aa0ae1515dce2dc
Joel Martinez <jmarti221@gmail.com>
2021-12-15 08:15:09 -0700

    Get autocomplete ingredients working

--- src/main/java/family/themartinez/mealplanner/controllers/ingredients/IngredientsController.java
+++ src/main/java/family/themartinez/mealplanner/controllers/ingredients/IngredientsController.java
@@ -1,7 +1,10 @@
 package family.themartinez.mealplanner.controllers.ingredients;
 
-import family.themartinez.mealplanner.data.ingredients.IdAndNameOnly;
 import family.themartinez.mealplanner.data.ingredients.IngredientRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +16,17 @@ public class IngredientsController {
   @Autowired private IngredientRepository ingredientRepository;
 
   @GetMapping("/ingredients")
-  public Iterable<IdAndNameOnly> getIngredients(@RequestParam(name = "q") String query) {
-    return ingredientRepository.findTop10ByNameLikeIgnoreCase(String.format("%%%s%%", query));
+  public List<Map<String, String>> getIngredients(@RequestParam(name = "q") String query) {
+    return ingredientRepository
+        .findTop10ByNameLikeIgnoreCase(String.format("%%%s%%", query))
+        .stream()
+        .map(
+            e -> {
+              Map<String, String> map = new HashMap<>();
+              map.put("value", e.getId().toString());
+              map.put("text", e.getName());
+              return map;
+            })
+        .collect(Collectors.toList());
   }
 }
