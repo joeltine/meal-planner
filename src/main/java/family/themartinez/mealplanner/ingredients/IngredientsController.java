e712831635de5a07277333fe185457d7cb0e554f
Joel Martinez <jmarti221@gmail.com>
2021-12-14 17:31:39 -0700

    Make IngredientsController sql query compatible with H2 (which uses "value" as a primitive)

--- src/main/java/family/themartinez/mealplanner/ingredients/IngredientsController.java
+++ src/main/java/family/themartinez/mealplanner/ingredients/IngredientsController.java
@@ -1,7 +1,9 @@
 package family.themartinez.mealplanner.ingredients;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +19,24 @@ public class IngredientsController {
   }
 
   @GetMapping("/ingredients")
-  public List<Map<String, Object>> getIngredients(@RequestParam(name = "q") String query) {
-    return this.jdbcTemplate.queryForList(
-        "SELECT id AS value, name AS text FROM ingredients WHERE name LIKE ? LIMIT 10",
-        new String[] {String.format("%%%s%%", query)});
+  public List<Map<String, String>> getIngredients(@RequestParam(name = "q") String query) {
+    return this.jdbcTemplate
+        .queryForList(
+            "SELECT id, name FROM ingredients WHERE name LIKE ? LIMIT 10",
+            new String[] {String.format("%%%s%%", query)})
+        .stream()
+        .map(
+            e -> {
+              Map<String, String> map = new HashMap<>();
+              for (Map.Entry x : e.entrySet()) {
+                if (x.getKey().equals("id")) {
+                  map.put("value", (String) x.getValue());
+                } else if (x.getKey().equals("name")) {
+                  map.put("text", (String) x.getValue());
+                }
+              }
+              return map;
+            })
+        .collect(Collectors.toList());
   }
 }
