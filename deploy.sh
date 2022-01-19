a0bc4ea642c6c4eea016398f774fbec192ab2fc2
Joel Martinez <jmarti221@gmail.com>
2022-01-18 22:10:17 -0700

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-ssh joel@192.168.1.211 << EOF
+ssh -T joel@192.168.1.211 << EOF
   cd /opt/meal-planner
   git pull
-  ./mvnw clean package -DskipTests
-  sudo systemctl restart meal-planner.service
+  ./mvnw clean package -DskipTests && sudo systemctl restart meal-planner.service
 EOF
