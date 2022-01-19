9096d968cdb59e7507be93621f27d4761d6a0510
Joel Martinez <jmarti221@gmail.com>
2022-01-19 06:57:43 -0700

    Read scrape_recipe.py from Filesystem instead of as a resource in JAR

--- src/main/java/family/themartinez/mealplanner/scraper/ExternalRecipeScraper.java
+++ src/main/java/family/themartinez/mealplanner/scraper/ExternalRecipeScraper.java
@@ -5,26 +5,18 @@ import com.google.common.io.CharStreams;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
 import org.springframework.data.util.Pair;
 
 public class ExternalRecipeScraper {
 
-  private File scrapeRecipeScript;
+  private final File scrapeRecipeScript;
 
-  private static String PYTHON = "/usr/bin/python3";
+  private static final String PYTHON = "/usr/bin/python3";
 
-  public ExternalRecipeScraper() throws IOException {
-    try {
-      URL scrapeRecipePyUrl = getClass().getClassLoader().getResource("scripts/scrape_recipe.py");
-      if (scrapeRecipePyUrl == null) {
-        throw new IllegalArgumentException("scrape_recipe.py not found!");
-      } else {
-        scrapeRecipeScript = new File(scrapeRecipePyUrl.toURI());
-      }
-    } catch (Exception e) {
-      System.out.println(e);
-    }
+  private static final String SCRAPER_PY = "src/main/resources/scripts/scrape_recipe.py";
+
+  public ExternalRecipeScraper() {
+    scrapeRecipeScript = new File(SCRAPER_PY);
   }
 
   /**
