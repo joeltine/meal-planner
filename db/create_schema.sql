1b7c83c190d588ac3684c0470bb05b5ebe340b40
Joel Martinez <jmarti221@gmail.com>
2022-01-25 07:27:15 -0700

    Add user creation to schema script

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -2,6 +2,8 @@ create schema meal_planner_dev collate utf8_general_ci;
 
 create schema meal_planner collate utf8_general_ci;
 
+CREATE USER IF NOT EXISTS 'mp-dev'@'192.168.1.%' IDENTIFIED BY 'changeme';
+
 grant delete, insert, select, trigger, update on meal_planner_dev.* to 'mp-dev'@'192.168.1.%';
 
 grant delete, insert, select, trigger, update on meal_planner.* to 'mp-dev'@'192.168.1.%';
